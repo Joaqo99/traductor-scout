fbe8c8bcea393f791e595f4ddac1f64091348bc7
Joaqo99 <joaqo9909@gmail.com>
2021-12-20 21:35:02 -0300

    Arreglo de clave Paquidermo

--- claves/paquidermo.js
+++ claves/paquidermo.js
@@ -1,38 +1,16 @@
 function paquidermo_encriptar(mensaje){
 
     for (i of mensaje){
-        switch(i){
-            case "p": 
-                mensaje = mensaje.replace(i, "0");
-                break
-            case "a": 
-                mensaje = mensaje.replace(i, "1");
-                break
-            case "q": 
-                mensaje = mensaje.replace(i, "2");
-                break
-            case "u": 
-                mensaje = mensaje.replace(i, "3");
-                break
-            case "i": 
-                mensaje = mensaje.replace(i, "4");
-                break
-            case "d": 
-                mensaje = mensaje.replace(i, "5");
-                break
-            case "e": 
-                mensaje = mensaje.replace(i, "6");
-                break
-            case "r": 
-                mensaje = mensaje.replace(i, "7");
-                break
-            case "m": 
-                mensaje = mensaje.replace(i, "8");
-                break
-            case "o": 
-                mensaje = mensaje.replace(i, "9");
-                break
-        }
+        if (i == "p" || i == "P"){mensaje = mensaje.replace(i, "0");} 
+        if (i == "a" || i == "A" || i == "á" || i == "Á"){mensaje = mensaje.replace(i, "1");} 
+        if (i == "q" || i == "Q"){mensaje = mensaje.replace(i, "2");} 
+        if (i == "u" || i == "U" || i == "ú" || i == "Ú"|| i == "Ü" || i == "ü"){mensaje = mensaje.replace(i, "3");} 
+        if (i == "i" || i == "I" || i == "í" || i == "Í"){mensaje = mensaje.replace(i, "4");} 
+        if (i == "d" || i == "D"){mensaje = mensaje.replace(i, "5");} 
+        if (i == "e" || i == "E" || i == "é" || i == "É"){mensaje = mensaje.replace(i, "6");} 
+        if (i == "r" || i == "R"){mensaje = mensaje.replace(i, "7");} 
+        if (i == "m" || i == "M"){mensaje = mensaje.replace(i, "8");} 
+        if (i == "o" || i == "O" || i == "ó" || i == "Ó"){mensaje = mensaje.replace(i, "9");} 
     }
 
     return mensaje
