b96a57cc3a83de475901971e197a22b5e2584aa3
Joaqo99 <joaqo9909@gmail.com>
2022-01-08 11:05:42 -0300

    boton encriptado

--- codigo.js
+++ codigo.js
@@ -7,9 +7,15 @@ let boton_encriptar = document.getElementById("encriptar")
 
 /* Crear el evento para el boton traducir: obtener el input.value, traducir, y mostrar el mensaje en el div: */
 
+let encriptar = boton_encriptar.checked
+
+boton_encriptar.addEventListener("onChange", ()=>{
+    encriptar = !encriptar
+})
+
 boton_traducir.addEventListener("click", ()=>{
     let mensaje = input.value
-    if (boton_encriptar.checked){
+    if (encriptar){
         output.textContent = murcielago_encriptar(mensaje)
     }else{
         output.textContent = murcielago_desencriptar(mensaje)
