2169c66a69c0cd30c9f92d67cc6cb0aeae008e12
Joaqo99 <joaqo9909@gmail.com>
2022-01-15 17:32:54 -0300

    etiquetas hechas

--- codigo.js
+++ codigo.js
@@ -4,6 +4,8 @@ let input = document.getElementById("mensaje-entrada")
 let output = document.getElementById("mensaje-salida")
 let boton_traducir = document.querySelector(".boton-traducir")
 let boton_encriptar = document.getElementById("encriptar")
+let etiqueta_desencriptar = document.querySelector(".etiqueta-desencriptar")
+let etiqueta_encriptar = document.querySelector(".etiqueta-encriptar")
 
 /* Crear el evento para el boton traducir: obtener el input.value, traducir, y mostrar el mensaje en el div: */
 
@@ -11,6 +13,13 @@ let encriptar = boton_encriptar.checked
 
 boton_encriptar.addEventListener("change", ()=>{
     encriptar = !encriptar
+    if(encriptar){
+        etiqueta_desencriptar.classList.remove("etiqueta-active-des")
+        etiqueta_encriptar.classList.add("etiqueta-active-en")
+    }else{
+        etiqueta_encriptar.classList.remove("etiqueta-active-en")
+        etiqueta_desencriptar.classList.add("etiqueta-active-des")
+    }
 })
 
 boton_traducir.addEventListener("click", ()=>{
